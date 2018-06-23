88a012efec668d0fd3237b2cde11232ffebf4192
Keri Hoadley <keri.hoadley@gmail.com>
2018-06-22 18:30:21 -0600

    Final version of grating approximations

--- kcrm/kcrm_gratings_v3.py
+++ kcrm/kcrm_gratings_v3.py
@@ -138,24 +138,43 @@ D = 155		# mm
 
 ########### Grating Sets ############
 ##
+## Low: RH1
+#~ gtg = 'KCRM RL v1'
+#~ gtg_ind = ['KCRM RL1 v1']
+#~ res_avg = 800
+#~ r_suite = numpy.array( [0.514] )	# RL lines/micron
+#~ #alpha = numpy. array( [16.0, 14.0, 11.5] )# RL AOI
+#~ alpha = numpy. array( [18.0, 16.0, 13.79] )# RL AOI
+#~ #tilt_suite = numpy.array( [0.98] )				# Tilt of grating fringes from normal
+#~ tilt_suite = numpy.array( [2.4] )				# Tilt of grating fringes from normal
+#~ dn_suite = numpy.array( [0.05] )		# index of refraction modulation through gelatin
+#~ thick_suite = numpy.array( [7.07] )	# thickness of the gelatin layer, in microns(?)
+
+##
 ## Medium: RM1 + RM2
-gtg = 'KCRM RM v1'
-res_avg = 1900
-r_suite = numpy.array( [1.220, 0.921] )	# RM lines/micron
-alpha = numpy. array( [32.0, 29.0, 26.0, 23.0] )					# RM AOI
-tilt_suite = numpy.array( [1.98, 1.08] )	#2.5			# Tilt of grating fringes from normal
-dn_suite = numpy.array( [0.04, 0.05] )		# index of refraction modulation through gelatin
-thick_suite = numpy.array( [8.42, 9.47] )	# thickness of the gelatin layer, in microns(?)
+#~ gtg = 'KCRM RM v1'
+#~ gtg_ind = ['KCRM RM1 v1','KCRM RM2 v1']
+#~ res_avg = 1900
+#~ r_suite = numpy.array( [1.220, 0.921] )	# RM lines/micron
+#~ alpha = numpy. array( [32.0, 28.0, 24.0] )				#32.0, 	# RM AOI
+#~ #alpha = numpy. array( [29.0, 26.0, 23.0] )				#32.0, 	# RM AOI
+#~ tilt_suite = numpy.array( [2.5, 2.7] )	#2.5			# Tilt of grating fringes from normal
+#~ #tilt_suite = numpy.array( [1.85, 0.93] )	#2.5			# Tilt of grating fringes from normal
+#~ dn_suite = numpy.array( [0.04, 0.05] )		# index of refraction modulation through gelatin
+#~ thick_suite = numpy.array( [8.42, 9.47] )	# thickness of the gelatin layer, in microns(?)
 
 ##
 ## High: RH1 + RH2 + RH3 + RH4
 gtg = 'KCRM RH v1'
+gtg_ind = ['KCRM RH1 v1','KCRM RH2 v1', 'KCRM RH3 v1','KCRM RH4 v1']
 res_avg = 4600
-r_suite = numpy.array( [2.42, 2.03, 1.705, 1.435] )		# RH1 lines/micron
-alpha = numpy.array( [56., 53., 50., 48.] )						# RH AOI
-tilt_suite = numpy.array( [3.27, 4.05, 4.36, 2.9] )-1.0	#2.5			# Tilt of grating fringes from normal
+r_suite = numpy.array( [2.52, 2.10, 1.725, 1.45] )		# RH1 lines/micron
+alpha = numpy.array( [56., 52.5, 50., 47.5] )						# RH AOI
+#alpha = numpy.array( [56., 53., 50., 48.] )						# RH AOI
+tilt_suite = numpy.array( [1.0,1.0,1.0,1.0] )+1
+#tilt_suite = numpy.array( [3.39, 4.165, 4.44, 3.01] )-1.0	#2.5			# Tilt of grating fringes from normal
 dn_suite = numpy.array( [0.08, 0.09, 0.09, 0.09] )			# index of refraction modulation through gelatin
-thick_suite = numpy.array( [8.88, 8.98,10.48, 13.61] )
+thick_suite = numpy.array( [8.5, 9.08, 11.08, 13.18] )
 
 
 
@@ -165,7 +184,7 @@ eta = numpy.zeros( [len(r_suite),len(alpha),numpy.size(waves)] )
 braggs_eff = numpy.zeros( [len(r_suite),numpy.size(waves)] )
 resol = numpy.zeros( [len(r_suite),numpy.size(waves)] )
 
-fig1 = plt.figure(figsize=(16,6))
+fig1 = plt.figure(figsize=(6*numpy.size(r_suite),8))
 
 # Loop through each grating in suite:
 for g in range(0,numpy.size(r_suite)):
@@ -173,6 +192,9 @@ for g in range(0,numpy.size(r_suite)):
 	tilt = tilt_suite[g]
 	dn = dn_suite[g]
 	thick = thick_suite[g]
+
+	# make subplot for index in grating suite
+	ax = fig1.add_subplot(1,numpy.size(r_suite),g+1)
 
 	# Define global variables to call in functions
 	global d2r, K, phi, delta
@@ -200,7 +222,7 @@ for g in range(0,numpy.size(r_suite)):
 
 #
 # 2.
-# Deteermine efficiency per angle setting, and over al angles per wavelength
+# Determine efficiency per angle setting, and over al angles per wavelength
 # A: Per angle setting (set alpha; use tilt to determine beta and efficiency)
 	for i in range(0,len(alpha)):
 		theta = math.asin( math.sin(alpha[i]*d2r)/nref )
@@ -234,9 +256,13 @@ for g in range(0,numpy.size(r_suite)):
 	# Print 70% encapsulated area (where eff >= 70%)
 	i_mx = numpy.where(braggs_eff[g] == numpy.max(braggs_eff[g]))[0]
 	brg_mx = math.asin( nref * math.sin( math.asin(waves[i_mx] * r/2./nref) + delta ) )/d2r	#math.asin(waves[i_mx]*r/2./nref + delta)/d2r
-	i_70 = numpy.where(braggs_eff[g]>0.7)[0]
+	if numpy.size(r_suite) == 4:
+		cutoff = 0.5
+	else:
+		cutoff = 0.7
+	i_70 = numpy.where(braggs_eff[g]>cutoff)[0]
 	if len(i_70) == 0:
-		i_70 = numpy.where(braggs_eff[g] > numpy.max(braggs_eff[g])*0.7)[0]
+		i_70 = numpy.where(braggs_eff[g] > numpy.max(braggs_eff[g])*cutoff)[0]
 	i_70_min = i_70[0]
 	i_70_max = i_70[-1]
 	wv_mn = waves[i_70_min]
@@ -257,45 +283,78 @@ for g in range(0,numpy.size(r_suite)):
 	plt.xlim(500.,1100.)
 	plt.ylim(0.0,1.1)
 	plt.xlabel('Wavelength (nm)',fontsize=14)
-	plt.ylabel('Theoretical Efficiency',fontsize=14)
+	if g == 0:
+		plt.ylabel('Theoretical Efficiency',fontsize=14)
 	plt.axhline(0.7, color='darkblue', linestyle='dashed', linewidth=3)
+	plt.axhline(0.5, color='darkblue', linestyle='dashed', linewidth=1)
 	plt.axvline(530., color='purple', linestyle=':')
 	plt.axvline(1080., color='purple', linestyle=':')
-	plt.title(gtg,fontsize=20)
+	plt.title(gtg_ind[g],fontsize=18)#(gtg,fontsize=20)
 	#~ plt.grid('True')
 	# Angles
 	for k in range(0,len(alpha)):
-		plt.text(wv0[g,k]*1000 - 10, braggs_eff[g,numpy.where(waves==wv0[g,k])[0]]*1.055,
-					 r'$\alpha$ = %0.2f$^{\circ}$'%(alpha[k]), color='grey' )
-		plt.text(wv0[g,k]*1000 - 10, braggs_eff[g,numpy.where(waves==wv0[g,k])[0]]*1.02,
-					 r'$\beta$ = %0.2f$^{\circ}$'%(beta[g,k]), color='grey' )
+		if abs(alpha[k] - beta[g,k]) < 6.5:
+			clr = 'crimson'
+		else:
+			clr = 'darkgreen'#'grey'
+		plt.text(wv0[g,k]*1000 - 10, braggs_eff[g,numpy.where(waves==wv0[g,k])[0]]*1.05,
+					 r'$\alpha$ = %0.2f$^{\circ}$'%(alpha[k]), color=clr, fontsize = 8 )
+		plt.text(wv0[g,k]*1000 - 10, braggs_eff[g,numpy.where(waves==wv0[g,k])[0]]*1.025,
+					 r'$\beta$ = %0.2f$^{\circ}$'%(beta[g,k]), color=clr, fontsize = 8 )
 	# Add grating parms in text
-	if len(r_suite) == 1:
+	if len(r_suite) > 0:	#== 1:
 		plt.text(550,0.425, r'$\rho$ = %i lines/mm'%(r*1000.),
 				 bbox=dict(facecolor='white', edgecolor='white', alpha=0.9),
-				 fontsize = 12)
+				 fontsize = 10)
 		plt.text(550,0.35, r'$\lambda_c$ = %i nm'%(waves[i_mx]*1000),
 				 bbox=dict(facecolor='white', edgecolor='white', alpha=0.9),
-				 fontsize = 12)
-		plt.text(650,0.35, r'$\lambda_{min}$ = %i nm'%(wv_mn*1000),
+				 fontsize = 10)
+		plt.text(750,0.35, r'$\lambda_{min}$ = %i nm'%(wv_mn*1000),
 				 bbox=dict(facecolor='white', edgecolor='white', alpha=0.9),
-				 fontsize = 12)
+				 fontsize = 10)
 		plt.text(550,0.275, r'd = %0.2f $\mu$m'%(thick),
 				 bbox=dict(facecolor='white', edgecolor='white', alpha=0.9),
-				 fontsize = 12)
-		plt.text(650,0.275, r'$\lambda_{max}$ = %i nm'%(wv_mx*1000),
+				 fontsize = 10)
+		plt.text(750,0.275, r'$\lambda_{max}$ = %i nm'%(wv_mx*1000),
 				 bbox=dict(facecolor='white', edgecolor='white', alpha=0.9),
-				 fontsize = 12)
+				 fontsize = 10)
 		plt.text(550,0.20, r'$\phi$ = %0.2f$^{\circ}$'%(tilt),
 				 bbox=dict(facecolor='white', edgecolor='white', alpha=0.9),
-				 fontsize = 12)
+				 fontsize = 10)
 		plt.text(550,0.125, r'n = %0.2f'%(nref),
 				 bbox=dict(facecolor='white', edgecolor='white', alpha=0.9),
-				 fontsize = 12)
+				 fontsize = 10)
 		plt.text(550,0.05, r'$\Delta$n = %0.2f'%(dn),
 				 bbox=dict(facecolor='white', edgecolor='white', alpha=0.9),
-				 fontsize = 12)
-plt.show()
+				 fontsize = 10)
+	#~ elif len(r_suite) == 1:
+		#~ plt.text(550,0.425, r'$\rho$ = %i lines/mm'%(r*1000.),
+				 #~ bbox=dict(facecolor='white', edgecolor='white', alpha=0.9),
+				 #~ fontsize = 12)
+		#~ plt.text(550,0.35, r'$\lambda_c$ = %i nm'%(waves[i_mx]*1000),
+				 #~ bbox=dict(facecolor='white', edgecolor='white', alpha=0.9),
+				 #~ fontsize = 12)
+		#~ plt.text(650,0.35, r'$\lambda_{min}$ = %i nm'%(wv_mn*1000),
+				 #~ bbox=dict(facecolor='white', edgecolor='white', alpha=0.9),
+				 #~ fontsize = 12)
+		#~ plt.text(550,0.275, r'd = %0.2f $\mu$m'%(thick),
+				 #~ bbox=dict(facecolor='white', edgecolor='white', alpha=0.9),
+				 #~ fontsize = 12)
+		#~ plt.text(650,0.275, r'$\lambda_{max}$ = %i nm'%(wv_mx*1000),
+				 #~ bbox=dict(facecolor='white', edgecolor='white', alpha=0.9),
+				 #~ fontsize = 12)
+		#~ plt.text(550,0.20, r'$\phi$ = %0.2f$^{\circ}$'%(tilt),
+				 #~ bbox=dict(facecolor='white', edgecolor='white', alpha=0.9),
+				 #~ fontsize = 12)
+		#~ plt.text(550,0.125, r'n = %0.2f'%(nref),
+				 #~ bbox=dict(facecolor='white', edgecolor='white', alpha=0.9),
+				 #~ fontsize = 12)
+		#~ plt.text(550,0.05, r'$\Delta$n = %0.2f'%(dn),
+				 #~ bbox=dict(facecolor='white', edgecolor='white', alpha=0.9),
+				 #~ fontsize = 12)
+#~ plt.show()
+
+
 
 # Make channel curve for grating suite (mode):
 channel_eff = numpy.zeros( numpy.size(waves) )	# 1D array, so store highest efficiency of all gratings
@@ -305,24 +364,27 @@ for i in range(0,len(waves)):
 	else:
 		channel_eff[i] = numpy.max(braggs_eff[:,i])
 
-fig3 = plt.figure(figsize=(16,6))
+fig3 = plt.figure(figsize=(8,8))
+ax = fig3.add_subplot(2,1,1)
 plt.plot(waves*1000,channel_eff,color='black',linewidth=5)
 plt.xlim(500.,1100.)
 plt.ylim(0.0,1.1)
-plt.xlabel('Wavelength (nm)',fontsize=14)
+#~ plt.xlabel('Wavelength (nm)',fontsize=14)
 plt.ylabel('Theoretical Efficiency',fontsize=14)
 plt.axhline(0.7, color='darkblue', linestyle='dashed', linewidth=3)
+plt.axhline(0.5, color='darkblue', linestyle='dashed', linewidth=1)
 plt.axvline(530., color='purple', linestyle=':')
 plt.axvline(1080., color='purple', linestyle=':')
-plt.title(gtg,fontsize=20)
+plt.title(gtg+': Channel Envelope',fontsize=18)
 
 
 # resolution plot
-fig2 = plt.figure(figsize=(16,6))
+#~ fig2 = plt.figure(figsize=(16,6))
+ax = fig3.add_subplot(2,1,2)
 for g in range(0,len(r_suite)):
 	i_mx = numpy.where(braggs_eff[g] == numpy.max(braggs_eff[g]))[0]
 	brg_mx = math.asin( nref * math.sin( math.asin(waves[i_mx] * r/2./nref) + delta ) )/d2r	#math.asin(waves[i_mx]*r/2./nref + delta)/d2r
-	i_70 = numpy.where(braggs_eff[g]>0.7)[0]
+	i_70 = numpy.where(braggs_eff[g]>0.68)[0]
 	if len(i_70) == 0:
 		i_70 = numpy.where(braggs_eff[g] > numpy.max(braggs_eff[g])*0.7)[0]
 	i_70_min = i_70[0]
@@ -336,9 +398,10 @@ for g in range(0,len(r_suite)):
 
 	plt.plot(waves[i_70_min:i_70_max]*1000.,resol[g,i_70_min:i_70_max],color='black')
 	plt.xlim(500.,1100.)
-	plt.title(gtg,fontsize=20)
-	plt.xlabel('Wavelength (nm)',fontsize=14)
-	plt.ylabel(r'R ($\Delta \lambda$/$\lambda$)',fontsize=14)
+	if g == 0:
+		plt.xlabel('Wavelength (nm)',fontsize=14)
+		plt.ylabel(r'Resolution ($\Delta \lambda$/$\lambda$)',fontsize=14)
+		plt.title(gtg+': Resolution (70% Bandwidth)',fontsize=18)
 	plt.axvline(530., color='purple', linestyle=':')
 	plt.axvline(1080., color='purple', linestyle=':')
 	plt.axhline(res_avg, color='blue', linestyle='dashed', linewidth=3)
